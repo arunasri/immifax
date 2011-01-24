30f05d1454c596d06de5902d05091b240dc6d975
Neeraj Singh <neerajdotname@gmail.com>
2011-01-23 21:22:55 -0500

    wip first commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+
+group :development, :test do
+
+  gem 'ruby-debug'
+
+  gem 'mongrel'
+
+  gem 'capistrano', ">= 2.5.19"
+  gem 'capistrano-ext'
+
+  gem "factory_girl_rails"
+  gem "database_cleaner"
+  gem "shoulda"
+  gem "rspec-rails", ">= 2.0.1"
+  gem "cucumber-rails", ">= 0.3.2"
+  gem "capybara", "= 0.4.0"
+  gem "launchy"
+  gem "redgreen"
+  gem "faker"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisaInfo::Application.config.secret_token = 'deffcf57aa32f192a12d33c1659a2e75be6d6ca522ef9cbb4d1e0bfde7132b060b373b91e1c0b7756e940fbe0c871e9fd9e28e4414a59c72aae36f6595661970'
